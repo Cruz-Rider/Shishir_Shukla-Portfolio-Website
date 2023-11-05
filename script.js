66b51b0395c2ae4ee519626e2b3024760a74b5d1
Shishir Shukla <1999shishirshukla@gmail.com>
2023-11-05 21:45:46 +0530

    Update website design and content

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ $("#sidebar_list li").on('click', function () {
   $("#sidebar_list li.active").removeClass('active');
   $(this).addClass('active');
 });
+
+$("#light_mode").on('click', function () {
+  $("html").attr('data-bs-theme', 'light');
+  console.log("Light Mode");
+  $(this).toggleClass('d-none');
+  $("#dark_mode").toggleClass('d-none');
+});
+
+$("#dark_mode").on('click', function () {
+  $("html").attr('data-bs-theme', 'dark');
+  console.log("Dark Mode");
+  $(this).toggleClass('d-none');
+  $("#light_mode").toggleClass('d-none');
+});
